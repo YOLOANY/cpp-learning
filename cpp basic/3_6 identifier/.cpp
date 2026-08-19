1bc0f8cf90c91a085bf771693080bfea7342f45a
YOLOANY <3346380069@qq.com>
2026-08-19 21:04:35 +0800

    3-6 标识符

---
+++ cpp basic/3_6 identifier/.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+/*标识符
+定义：给变量、函数、类、结构体等起的名字
+   tip:只能由字母、数字、下划线_组成
+       不能以数字开头（1a ❌，a1 ✅）
+       区分大小写（Age ≠ age）
+       不能与关键字重名
+*/
+
+int main(){
+    int a12=1314;
+    int a=5;
+    int A=6;
+    cout<<a<<A<<endl;
+    int appleCount=0;
+    int bananaCount=0;
+    int xiguaCount=0;
+
+    return 0;
+}
