282383a725dbc1d94a890dc62b8e9613f4236af9
YOLOANY <3346380069@qq.com>
2026-08-19 21:15:04 +0800

    4-1 整型

---
+++ cpp basic/4_1 int_type/.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+/*
+short        2字节      2^16      (-32768 -> 32767)
+int          4字节      2^32      (-2147483648 -> 2147483647)
+long         4字节      2^32      (-2^31 -> 2^31-1)
+long long    8字节      2^64      (-2^63 -> 2^63-1)
+*/
+
+int main() {
+    short a = 32769;
+    int b = 2147483648;
+    long c = 1;
+    long long d = 1;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+    cout << "d = " << d << endl;
+
+    return 0;
+}
