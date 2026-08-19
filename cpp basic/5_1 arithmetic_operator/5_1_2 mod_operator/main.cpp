90469e7c2525ff13079c7aff097e919d2b755207
YOLOANY <3346380069@qq.com>
2026-08-19 22:07:57 +0800

    5-1-2 取模运算符

--- cpp basic/5_1 arithmetic_operator/5_1_2 mod_operator/main.cpp
+++ cpp basic/5_1 arithmetic_operator/5_1_2 mod_operator/main.cpp
@@ -1,9 +1,26 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main()
-{
-    // TODO: 填写本节代码练习
+ // 1、取模符号和被除数一致
+ // 2、取模运算符用于计算余数
+
+int main() {
+    int a = 100;
+    int b = 9;
+    cout << a % b << endl;   // 1
+
+    a = 100;
+    b = -9;
+    cout << a % b << endl;   // 1
+
+    a = -100;
+    b = 9;
+    cout << a % b << endl;   // -1
+
+    a = -100;
+    b = -9;
+    cout << a % b << endl;   // -1
 
     return 0;
 }
+
