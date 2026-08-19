810f5aaed148b33f423770190b0f2ab234e2007f
YOLOANY <3346380069@qq.com>
2026-08-20 00:16:04 +0800

    5-3 比较运算符

--- cpp basic/05、运算符/05_03 compare_operator/main.cpp
+++ cpp basic/05、运算符/05_03 compare_operator/main.cpp
@@ -1,9 +1,25 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main()
-{
-    // TODO: 填写本节代码练习
+/*
+==    等于
+!=    不等于
+<     小于
+>     大于
+<=    小于等于
+>=    大于等于
+*/
+
+int main() {
+    int a = 6;
+    int b = 9;
+
+    cout << (a == b) << endl;
+    cout << (a != b) << endl;
+    cout << (a > b) << endl;
+    cout << (a < b) << endl;
+    cout << (a >= b) << endl;
+    cout << (a <= b) << endl;
 
     return 0;
 }
