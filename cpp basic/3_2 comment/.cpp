44e907544dd4467dbcbd6a10ae210de74905501c
YOLOANY <3346380069@qq.com>
2026-08-19 19:49:32 +0800

    3-2 注释

---
+++ cpp basic/3_2 comment/.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // 你相信我吗
+    // 我当然相信
+    /*
+    你相信我吗
+    我当然相信
+    */
+    cout << "Hello, World!" << endl; // This comment is after a statement
+
+    return 0;
+}
