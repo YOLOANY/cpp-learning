0bb9ebf058932b2955947f3b407135092e8d46b9
YOLOANY <3346380069@qq.com>
2026-08-20 00:19:57 +0800

    5-5 逗号运算符

--- cpp basic/05、运算符/05_05 comma_operator/main.cpp
+++ cpp basic/05、运算符/05_05 comma_operator/main.cpp
@@ -1,9 +1,23 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main()
-{
-    // TODO: 填写本节代码练习
+int main() {
+    int a = 1;
+    a = (5 - 6, 8 + 9, 100 / 7);  // 逗号表达式，取最后一项结果：14
+    cout << a << endl;
+
+    // 1. 案例1：交换两个变量
+    int x = 4;
+    int y = 5;
+    int tmp = x;
+    x = y;
+    y = tmp;
+    cout << x << ' ' << y << endl;
+
+    // 逗号表达式写法交换
+    tmp = x, x = y, y = tmp;
+    cout << x << ' ' << y << endl;
 
     return 0;
 }
+
