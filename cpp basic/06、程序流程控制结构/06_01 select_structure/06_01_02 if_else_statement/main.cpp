bb34506202fc4ab3f4b1b0406186ffd534b1390c
YOLOANY <3346380069@qq.com>
2026-08-20 11:49:11 +0800

    6-1-2 if else 语句

--- cpp basic/06、程序流程控制结构/06_01 select_structure/06_01_02 if_else_statement/main.cpp
+++ cpp basic/06、程序流程控制结构/06_01 select_structure/06_01_02 if_else_statement/main.cpp
@@ -1,9 +1,21 @@
 ﻿#include <iostream>
+#include <string>   // 使用string字符串必须要这个头文件
 using namespace std;
 
-int main()
-{
-    // TODO: 填写本节代码练习
+int main() {
+    int x;
+    string y = "添砖";
+    cin >> x;
 
+    if (x & 1) {   // x & 1 判断是否奇数
+        cout << "x 是一个你太美数" << endl;
+        y = y + "Java";   // 字符串拼接
+    }
+    else {
+        cout << "x 是一个不太美数" << endl;
+    }
+
+    cout << y << endl;
     return 0;
 }
+
