ebbfdcf2a443ba0134be4e15a4171462a99c3ea5
YOLOANY <3346380069@qq.com>
2026-08-20 11:51:59 +0800

    6-1-3 else if 语句

--- cpp basic/06、程序流程控制结构/06_01 select_structure/06_01_03 else_if_statement/main.cpp
+++ cpp basic/06、程序流程控制结构/06_01 select_structure/06_01_03 else_if_statement/main.cpp
@@ -1,9 +1,25 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main()
-{
-    // TODO: 填写本节代码练习
+int main() {
+    int a;
+    cin >> a;
+
+    if (a == 0) {
+        cout << "我不喜欢你" << endl;
+    }
+    else if (a == 1) {
+        cout << "我喜欢你" << endl;
+    }
+    else if (a == 2) {
+        cout << "我很喜欢你" << endl;
+    }
+    else if (a == 3) {
+        cout << "我特别喜欢你" << endl;
+    }
+    else {
+        cout << "我超级喜欢你" << endl;
+    }
 
     return 0;
 }
