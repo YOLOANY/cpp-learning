dbc25ac1fd3219dbfaa217c707720b771a654f79
YOLOANY <3346380069@qq.com>
2026-08-20 11:59:11 +0800

    6-2-2 do…while 语句

--- cpp basic/06、程序流程控制结构/06_02 loop_structure/06_02_02 do_while/main.cpp
+++ cpp basic/06、程序流程控制结构/06_02 loop_structure/06_02_02 do_while/main.cpp
@@ -1,9 +1,34 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main()
-{
-    // TODO: 填写本节代码练习
+int main() {
+    /*
+    int a = 0;
+    do {
+        cout << a << endl;
+        a += 1;
+    } while (a < 3);
+
+    a = 0;
+    while (a < 3) {
+        cout << a << endl;
+        a += 1;
+    }
+    */
+
+    int a = 3;
+    do {
+        cout << a << endl;
+        a += 1;
+    } while (a < 3);
+    cout << "---" << endl;
+
+    a = 3;
+    while (a < 3) {
+        cout << a << endl;
+        a += 1;
+    }
 
     return 0;
 }
+
