bd3e7c6d3702e72bbdbc1f0e3c16acad1b53f9ed
YOLOANY <3346380069@qq.com>
2026-08-20 12:02:55 +0800

    6-2-3 for 语句

--- cpp basic/06、程序流程控制结构/06_02 loop_structure/06_02_03 for/main.cpp
+++ cpp basic/06、程序流程控制结构/06_02 loop_structure/06_02_03 for/main.cpp
@@ -3,7 +3,51 @@ using namespace std;
 
 int main()
 {
-    // TODO: 填写本节代码练习
+    int n;
+    cin >> n;
+
+    //写法1
+    int sum = 0;
+    for (int i = 1; i <= n; ++i) {
+        sum += i;
+    }
+    cout << sum << endl;
+
+
+    //写法2
+    sum = 0;
+    int i = 1;
+    for (; i <= n; ++i) {
+        sum += i;
+    }
+    cout << sum << endl;
+
+
+    //写法3
+    sum = 0;
+    for (sum = 0, i = 1; i <= n; ++i) {
+        sum += i;
+    }
+    cout << sum << endl;
+
+
+    //写法4 死循环（不要实际运行，会卡死）
+    /*
+    sum = 0;
+    for (sum = 0, i = 1; ; ++i) {
+        sum += i;
+    }
+    cout << sum << endl;
+    */
+
+
+    //写法5
+    sum = 0;
+    for (sum = 0, i = 1; i <= n; ) {
+        sum += i;
+        ++i;
+    }
+    cout << sum << endl;
 
     return 0;
 }
