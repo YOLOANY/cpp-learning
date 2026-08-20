774bc1ff6480067bca2a493a0ffa17bd7b385829
YOLOANY <3346380069@qq.com>
2026-08-20 17:30:29 +0800

    6-3-1 break

--- cpp basic/06、程序流程控制结构/06_03 jump_structure/06_03_01 break/main.cpp
+++ cpp basic/06、程序流程控制结构/06_03 jump_structure/06_03_01 break/main.cpp
@@ -1,9 +1,37 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main()
-{
-    // TODO: 填写本节代码练习
+int main() {
+    // 1. while 中的 break;
+    int cnt = 0;
+    while (1) {
+        cnt++;
+        cout << "当前次数为：" << cnt << endl;
+        if (cnt > 20) {
+            break;
+        }
+    }
+    cout << "----------------" << endl;
+
+    // 2. for 中的break;
+    cnt = 20;
+    for (int i = 1; ; ++i) {
+        cout << "当前次数为：" << i << endl;
+        if (i > cnt) {
+            break;
+        }
+    }
+
+    // 3. switch case 中的 break
+    int a = 1;
+    switch (a) {
+        case 1:
+            cout << "我喜欢你" << endl;
+            break;
+        case 2:
+            cout << "我不喜欢你" << endl;
+    }
 
     return 0;
 }
+
