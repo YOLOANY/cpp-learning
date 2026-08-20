287665664db0ef9280104485191e14540d7d2449
YOLOANY <3346380069@qq.com>
2026-08-20 11:54:38 +0800

    6-1-4 条件运算符

--- cpp basic/06、程序流程控制结构/06_01 select_structure/06_01_04 conditional_operator/main.cpp
+++ cpp basic/06、程序流程控制结构/06_01 select_structure/06_01_04 conditional_operator/main.cpp
@@ -1,9 +1,24 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main()
-{
-    // TODO: 填写本节代码练习
+int main() {
+    int a = 3, b = 4;
+    double c = 1.6;
+    int x = -1;
+
+    // if‑else 求a、b较大值
+    if (a > b) {
+        x = a;
+    }
+    else {
+        x = b;
+    }
+
+    // 三目运算符：条件 ? 条件为真取值 : 条件为假取值
+    x = (a > b ? a : b);
+
+    // 嵌套三目运算符
+    cout << ((a > b) ? ((a > c) ? a : c) : c) << endl;
 
     return 0;
 }
