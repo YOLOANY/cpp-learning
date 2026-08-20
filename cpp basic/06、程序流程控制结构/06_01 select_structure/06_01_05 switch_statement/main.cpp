aeef7efe987ac88829aa2aa8457e061be31fb57e
YOLOANY <3346380069@qq.com>
2026-08-20 11:55:54 +0800

    6-1-5 switch 语句

--- cpp basic/06、程序流程控制结构/06_01 select_structure/06_01_05 switch_statement/main.cpp
+++ cpp basic/06、程序流程控制结构/06_01 select_structure/06_01_05 switch_statement/main.cpp
@@ -1,9 +1,24 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main()
-{
-    // TODO: 填写本节代码练习
-
+int main() {
+    int a = 0;
+    cin >> a;
+    switch (a) {
+        case 0:
+            cout << "Zero" << endl;
+            break;
+        case 1:
+            cout << "One" << endl;
+            break;
+        case 2:
+            cout << "Two" << endl;
+            break;
+        case 3:
+            cout << "Three" << endl;
+            break;
+        default:
+            cout << "Beyond three or below zero" << endl;
+    }
     return 0;
 }
