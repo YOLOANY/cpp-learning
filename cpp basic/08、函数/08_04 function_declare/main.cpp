f882d8f27fb956e47b45f79a90890681c81594aa
YOLOANY <3346380069@qq.com>
2026-08-21 18:48:10 +0800

    8-4 函数的声明

--- cpp basic/08、函数/08_04 function_declare/main.cpp
+++ cpp basic/08、函数/08_04 function_declare/main.cpp
@@ -1,9 +1,34 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main()
-{
-    // TODO: 填写本节代码练习
+// 函数声明
+int add(int, int);
+void func2(int);
 
+void func1(int x) {
+    if (x <= 0) {
+        cout << x << endl;
+        }
+    cout << "func1:" << x << endl;        
+    func2(x - 1);
+}
+
+void func2(int x) {
+    cout << "func2:" << x << endl;
+    func1(x-1);
+}
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    int z = add(x, y);
+    cout << z << endl;
+    
+    func1(10);
     return 0;
 }
+
+int add(int a, int b) {
+    return a + b;
+}
+
