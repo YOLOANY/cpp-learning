07ace20e093525d79599ae82b926a72d8a05b7bc
YOLOANY <3346380069@qq.com>
2026-08-21 18:24:21 +0800

    8-1 函数的定义

--- cpp basic/08、函数/08_01 function_define/main.cpp
+++ cpp basic/08、函数/08_01 function_define/main.cpp
@@ -1,9 +1,37 @@
 ﻿#include <iostream>
 using namespace std;
 
+/*
+1、返回值类型
+2、函数名
+3、参数列表
+4、函数体
+5、return 表达式
+
+返回值类型 函数名(参数列表)
+{
+    函数体
+    return 表达式
+}
+*/
+
+int max(int a, int b)
+{
+    if (a > b) {
+        return a;
+    }
+    return b;
+}
+
+int max1(int a, int b)
+{
+    return (a > b) ? a : b;
+}
+
 int main()
 {
-    // TODO: 填写本节代码练习
+    //double ret = add(1, 7);
+    //cout << ret << endl;
 
     return 0;
 }
