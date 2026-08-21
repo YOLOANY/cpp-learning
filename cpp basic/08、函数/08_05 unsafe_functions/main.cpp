494e058dd554710ba5bd207198ec0686756914ff
YOLOANY <3346380069@qq.com>
2026-08-21 18:52:07 +0800

    8-5 非安全函数

---
+++ cpp basic/08、函数/08_05 unsafe_functions/main.cpp
@@ -0,0 +1,14 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    char str[10];
+    strcpy(str, "hahahha");
+
+    return 0;
+}
