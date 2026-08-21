7cd4c0667d371837d771c2eda170edd49bb1ea39
YOLOANY <3346380069@qq.com>
2026-08-21 18:41:24 +0800

    8-3 函数值传递

--- cpp basic/08、函数/08_03 function_value_pass/main.cpp
+++ cpp basic/08、函数/08_03 function_value_pass/main.cpp
@@ -1,9 +1,29 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main()
-{
-    // TODO: 填写本节代码练习
+/*
+| a | b | a | b | tmp |
+| 6 | 9 | 6 | 9 |  6  |
+*/
 
+void swap(int a, int b) {
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "---" << endl;
+    int tmp = a;
+    a = b;
+    b = tmp;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "---" << endl;
+}
+
+int main() {
+    int a = 6;
+    int b = 9;
+    swap(a, b);
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
     return 0;
 }
+
